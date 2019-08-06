e5f85c9ca9e435a32351d9d472164aabd3630276
Devender Singh <devender.singh@ixcnew.com>
2019-08-06 16:38:24 +0530

    Initial build

---
+++ DotnetCoreTest/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DotnetCoreTest.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
